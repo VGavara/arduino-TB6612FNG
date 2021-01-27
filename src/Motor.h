6592d5f6d956a1d641fb8ebb59987c4c45c2a04f
Vicente Gavara <vicente.gavara@nbn23.com>
2021-01-27 20:04:38 +0100

    Spinner class added; Motor class code moved from tb6612fng.* files to Motor.* files

---
+++ src/Motor.h
@@ -0,0 +1,71 @@
+// Motor.h
+// Header file for Motor class
+// Copyright (c) Vicente Gavara. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <Arduino.h>
+
+// Rotation directions
+enum Direction
+{
+    Clockwise = 1,
+    CounterClockwise = -1
+};
+
+// Arduino output to driver input pin mapping
+typedef struct
+{
+    pin_size_t in1;
+    pin_size_t in2;
+    pin_size_t pwm;
+    pin_size_t stby;
+} PinMap;
+
+// Interfaces a motor controlled by a TB6612FNG driver
+class Motor
+{
+public:
+    // Summary: Initializes the Motor class
+    // Param pinMap: Mapping of motor inputs and Arduino pins
+    // Note: Set pinMap.stby to 0 to disable driver standby management
+    // Note: The mapped Arduino pins will be initialized by this function
+    Motor(PinMap *pinMap);
+
+    // Summary: Runs the motor in a given direction at a given speed
+    // Param direction: Rotation direction
+    // Param speed: Rotation speed, from 1 to 255
+    // Note: Standby mode will be switched off if this function is invoked
+    void run(Direction direction, uint8_t speed);
+
+    // Summary: Stops the motor
+    // Note: Stopping just cuts the motor power supply,
+    //  therefore doesn't cancel the motor inertial rotation.
+    //  Use Brake() if you want to cancel the inertial rotation.
+    void stop();
+
+    // Summary: Short-brakes the motor
+    // Note: Short-braking cancels the inertial motor rotation,
+    //  therefore is an energy consuming process
+    void brake();
+
+    // Summary: Sets the driver in standby mode
+    // Note: This function does nothing if the class constructor
+    //  was called with io_map.stby set to 0
+    void standBy();
+
+private:
+    PinMap pinMap_;
+
+    void rotateCW_(PinMap *pinMap);
+    void rotateCCW_(PinMap *pinMap);
+    void setStandBy_(PinMap *pinMap);
+    void clearStandBy_(PinMap *pinMap);
+    void setRotationSpeed_(PinMap *pinMap, uint8_t speed);
+    void stopRotation_(PinMap *pinMap);
+    void brakeRotation_(PinMap *pinMap);
+};
+
+#endif
---
+++ src/Spinner.h
@@ -0,0 +1,97 @@
+// Spinner.h
+// Header file for Spinner class
+// Copyright (c) Vicente Gavara. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef SPINNER_H
+#define SPINNER_H
+
+#include "Motor.h"
+
+/**
+ * Spin point
+ * @typedef {struct} SpinPoint
+ * @property {byte}} speed - Spin speed, from 1 to 255
+ * @property {unsigned int}} time - Elapsed time from the spin start, in milliseconds, in which speed must be reached
+ * @example A spinPoint with value {20,100} indicates that, after 100 milliseconds of start spinning, a speed of 20 must be reached
+ */
+struct SpinPoint
+{
+    byte speed;
+    unsigned int time;
+};
+
+/**
+ * Spinner callback type
+ * @typedef {(*)(const SpinPoint *)} SpinnerCB
+ */
+typedef void (*SpinnerCB)(const SpinPoint *);
+
+/**
+ * Controller for Toshiba TB6612FNG driven motors supporting acceleration/deleration features
+ * @class
+ */
+class Spinner
+{
+public:
+    /**
+     * Creates an acceleration/deceleration controller for TB6612FNG driven motors
+     * @constructor
+     * @param {Motor*} motor - Pointer to a Motor object instance
+     */
+    Spinner(Motor *motor);
+
+    /**
+     * Creates an acceleration/deceleration controller for TB6612FNG driven motors and use callbacks for handling its events
+     * @constructor
+     * @param {Motor*} motor - Pointer to a Motor object instance
+     * @param {SpinnerCB} spinUpdated -Callback function for handling the spin updated event
+     * @param {SpinnerCB} spinFinished - Callback function for handling the spin finished event
+     */
+    Spinner(Motor *motor, SpinnerCB spinUpdated, SpinnerCB spinFinished);
+
+    /**
+     * Starts a motor lineal acceleration/deceleration
+     * @param {Direction} direction - Motor rotation direction
+     * @param {SpinPoint[]} spinMap - Spin map containing just two points: the start and end spin points
+     * @returns {const SpinPoint*} Pointer to a constant SpinPoint struct containing the first spin map point or null if the start operation cannot be executed
+     * @note Only maps with two spin points are supported in this version. The first spin point must have the speed value set to the initial spin speed and the time value set to zero, the second and last spin point must have the speed value set to the final spin spped and the time value set to the spin ellapsed time
+     * @note Starting a spin operation will abort a previous running spinning operation
+     */
+    const SpinPoint *start(Direction direction, SpinPoint spinMap[]);
+
+    /**
+     * Updates a running spin operation
+     * @returns {const SpinPoint*} Pointer to a constant SpinPoint struct containing the last reached spin map point, or null if no spin operation is in progress.
+     * @note This function must be called periodically. To achieve a smooth spin, a call periodicity lower or equal to 1 millisecond is recommended.
+     */
+    const SpinPoint *spin();
+
+    /**
+     * Aborts a spin operation, keeping the motor rotating at the last speed reached by the aborted spin operation.
+     * @returns Pointer to a SpinPoint struct containing the last reached spin map point, or null if no spin operation was aborted.
+     */
+    const SpinPoint *abort();
+
+private:
+    Motor *motor_;
+
+    SpinPoint *map_;
+    unsigned short mapSize_;
+
+    Direction spinDirection_;
+    unsigned long spinStartTime_;
+    SpinPoint currentSpinPoint_;
+
+    unsigned short currentMapPointIndex_;
+
+    SpinnerCB spinUpdatedCB_, spinFinishedCB_;
+
+    bool checkSpinMap_(SpinPoint[], unsigned short);
+    unsigned long getElapsedTime_(unsigned long);
+    void updateSpeed_(Motor *, Direction, SpinPoint *, SpinnerCB);
+    unsigned short refreshSpinCourse_(SpinPoint *, unsigned short, unsigned long);
+    unsigned int getLinePointY_(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, unsigned int x);
+};
+
+#endif
